5c507fffb555e447fcaf2b8571d5652533a27fb6
voltyea <volty764@gmail.com>
2025-05-29 21:57:20 +0530

    added the update script

--- install.sh
+++ install.sh
@@ -1,8 +1,55 @@
 #!/usr/bin/bash
 
+if rfkill list wlan | grep -q "Soft blocked: yes"; then
 rfkill unblock wlan
+fi
+
+if rfkill list bluetooth | grep -q "Soft blocked: yes"; then
 rfkill unblock bluetooth
+fi
 
-sudo pacman -Syu --needed my_packages
+ xargs -a ./my_packages sudo pacman -Syu --needed
 
-chsh $USER
+# adding chaotic-aur
+
+if [ ! -f /etc/pacman.d/chaotic-mirrorlist ]; then 
+sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+sudo pacman-key --lsign-key 3056513887B78AEB
+
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' 
+
+ if ! grep -qF "[chaotic-aur]" /etc/pacman.conf; then
+  echo -e "\n\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist" | sudo tee -a /etc/pacman.conf
+  sudo pacman -Syu
+  fi
+fi
+
+#Pacman stuff
+if ! grep -qF "ILoveCandy" /etc/pacman.conf; then
+  echo -e "\n\n[options]\nILoveCandy" | sudo tee -a /etc/pacman.conf
+fi
+
+#Changing shell to fish
+
+FISH_PATH=$(command -v fish)
+CURRENT_SHELL=$(basename "$SHELL")
+
+if [ "$CURRENT_SHELL" != "fish" ]; then
+  chsh -s "$FISH_PATH" $USER
+fi
+
+#changing grub timeout
+search_line="GRUB_TIMEOUT=5"
+file="/etc/default/grub"
+
+if grep -Fxq "$search_line" "$file"; then
+  
+  sudo sed -i 's/^GRUB_TIMEOUT=5$/GRUB_TIMEOUT=0/' /etc/default/grub
+
+  sudo grub-mkconfig -o /boot/grub/grub.cfg
+
+fi
+
+#adding insults to sudo
+echo "Defaults insults" | sudo tee /etc/sudoers.d/insults
