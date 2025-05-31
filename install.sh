bca43ecd0ed2a0d6a416b9f8e87c5f5b5a144b69
voltyea <volty764@gmail.com>
2025-05-31 17:10:26 +0530

    commit

--- install.sh
+++ install.sh
@@ -3,6 +3,29 @@
 rfkill unblock wlan
 rfkill unblock bluetooth
 
+#adding insults to sudo
+echo "Defaults insults" | sudo tee /etc/sudoers.d/insults
+
+#Pacman stuff
+if ! grep -qF "ILoveCandy" /etc/pacman.conf; then
+  echo -e "\n\n[options]\nILoveCandy" | sudo tee -a /etc/pacman.conf
+fi
+
+# adding chaotic-aur
+
+if [ ! -f /etc/pacman.d/chaotic-mirrorlist ]; then
+  sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+  sudo pacman-key --lsign-key 3056513887B78AEB
+
+  sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
+  sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+
+  if ! grep -qF "[chaotic-aur]" /etc/pacman.conf; then
+    echo -e "\n\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist" | sudo tee -a /etc/pacman.conf
+    sudo pacman -Syu
+  fi
+fi
+
 sudo pacman -S --needed --noconfirm rate-mirrors
 sudo chmod +x ./update
 sudo cp ./update /usr/bin
@@ -14,33 +37,45 @@ xargs -a ./aur_packages yay -Syu --needed --noconfirm
 #cpu stuff
 vendor=$(grep -m 1 'vendor_id' /proc/cpuinfo | awk '{print $3}')
 case "$vendor" in
-    GenuineIntel)
-        sudo pacman -S --needed --noconfirm intel-ucode vulkan-intel lib32-vulkan-intel
-        ;;
-    AuthenticAMD)
-        sudo pacman -S --needed --noconfirm amd-ucode vulkan-radeon lib32-vulkan-radeon
-        ;;
+GenuineIntel)
+  sudo pacman -S --needed --noconfirm intel-ucode vulkan-intel lib32-vulkan-intel
+  ;;
+AuthenticAMD)
+  sudo pacman -S --needed --noconfirm amd-ucode vulkan-radeon lib32-vulkan-radeon
+  ;;
 esac
 
-# adding chaotic-aur
+#changing grub timeout
+search_line="GRUB_TIMEOUT=5"
+file="/etc/default/grub"
 
-if [ ! -f /etc/pacman.d/chaotic-mirrorlist ]; then 
-sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
-sudo pacman-key --lsign-key 3056513887B78AEB
+if grep -Fxq "$search_line" "$file"; then
 
-sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
-sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' 
+  sudo sed -i 's/^GRUB_TIMEOUT=5$/GRUB_TIMEOUT=0/' /etc/default/grub
 
- if ! grep -qF "[chaotic-aur]" /etc/pacman.conf; then
-  echo -e "\n\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist" | sudo tee -a /etc/pacman.conf
-  sudo pacman -Syu
-  fi
 fi
 
-#Pacman stuff
-if ! grep -qF "ILoveCandy" /etc/pacman.conf; then
-  echo -e "\n\n[options]\nILoveCandy" | sudo tee -a /etc/pacman.conf
+#Copying dotfiles
+version_gt() {
+  [ "$(printf '%s\n%s\n' "$1" "$2" | sort -V | head -n1)" != "$1" ]
+}
+github_version=$(curl -s https://raw.githubusercontent.com/voltyea/dotfiles/main/VERSION.txt)
+local_version=""
+if [[ -f "$HOME/.config/VERSION.txt" ]]; then
+  local_version=$(cat "$HOME/.config/VERSION.txt")
 fi
+if version_gt "$github_version" "$local_version"; then
+  git clone https://github.com/voltyea/dotfiles.git /tmp/dotfiles
+  sudo cp -r /tmp/dotfiles/* ~/.config
+fi
+
+#starting services
+sudo systemctl enable sddm.service
+sudo systemctl enable NetworkManager.service
+
+#regenerating mkinitcpio and the grub config
+sudo mkinitcpio -P
+sudo grub-mkconfig -o /boot/grub/grub.cfg
 
 #Changing shell to fish
 
@@ -51,23 +86,19 @@ if [ "$CURRENT_SHELL" != "fish" ]; then
   chsh -s "$FISH_PATH" $USER
 fi
 
-#changing grub timeout
-search_line="GRUB_TIMEOUT=5"
-file="/etc/default/grub"
+#rebooting the system
+# Prompt the user
+read -p "Do you want to reboot the system now? (yes/no): " answer
 
-if grep -Fxq "$search_line" "$file"; then
-  
-  sudo sed -i 's/^GRUB_TIMEOUT=5$/GRUB_TIMEOUT=0/' /etc/default/grub
+# Convert answer to lowercase
+answer=$(echo "$answer" | tr '[:upper:]' '[:lower:]')
 
-  sudo grub-mkconfig -o /boot/grub/grub.cfg
-
+# Check the user's input
+if [[ "$answer" == "yes" || "$answer" == "y" ]]; then
+  echo "Rebooting now..."
+  sudo reboot
+elif [[ "$answer" == "no" || "$answer" == "n" ]]; then
+  echo "Reboot canceled."
+else
+  echo "Invalid input. Please enter yes or no."
 fi
-
-#adding insults to sudo
-echo "Defaults insults" | sudo tee /etc/sudoers.d/insults
-
-#coping dotfiles
-
-
-#starting services
-sudo systemctl enable sddm.service
