0186c51100923585419894d1663fdc2aed96e19e
voltyea <volty764@gmail.com>
2025-06-03 18:38:45 +0530

    commit

--- sddm.sh
+++ sddm.sh
@@ -4,3 +4,5 @@ sudo mkdir -p /etc/sddm.conf.d/
 sudo cp dotfiles/sddm/sddm.conf /etc/sddm.conf.d/
 sudo cp -r dotfiles/sddm/themes/corners/ /usr/share/sddm/themes/
 sudo cp dotfiles/sddm/user_face_icons/volty.face.icon /usr/share/sddm/faces/
+sudo mkdir -p /etc/X11/xorg.conf.d/
+sudo cp ./30-touchpad.conf /etc/X11/xorg.conf.d/
