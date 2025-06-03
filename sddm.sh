76ed9424f61615179f098db40a64ec958579d9a5
voltyea <volty764@gmail.com>
2025-06-03 11:24:29 +0530

    Update sddm.sh

--- sddm.sh
+++ sddm.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
 
+sudo mkdir -p /etc/sddm.conf.d/
 sudo cp dotfiles/sddm/sddm.conf /etc/sddm.conf.d/
 sudo cp -r dotfiles/sddm/themes/corners/ /usr/share/sddm/themes/
 sudo cp dotfiles/sddm/user_face_icons/volty.face.icon /usr/share/sddm/faces/
