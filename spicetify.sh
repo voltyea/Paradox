fccd5e690b38047717f16a900462d89cdb8bf596
voltyea <volty764@gmail.com>
2025-06-11 21:45:48 +0530

    commit

--- spicetify.sh
+++ spicetify.sh
@@ -5,5 +5,3 @@ sudo mkdir -p /opt/spotify/Apps
 
 sudo chmod a+wr /opt/spotify
 sudo chmod a+wr /opt/spotify/Apps -R
-
-curl -fsSL https://raw.githubusercontent.com/spicetify/cli/main/install.sh | sh
